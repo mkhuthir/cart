2bd964c97009ec364522d08a83c88cc44ed2ebb2
mkhuthir <mkhuthir@gmail.com>
2022-04-17 09:34:23 +0400

    u

--- launch/cart_velocity.launch.py
+++ launch/cart_velocity.launch.py
@@ -1,29 +1,11 @@
-# Copyright 2020 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
-
 import xacro
 
 
@@ -34,7 +16,7 @@ def generate_launch_description():
              )
 
     gazebo_ros2_control_demos_path = os.path.join(
-        get_package_share_directory('gazebo_ros2_control_demos'))
+        get_package_share_directory('gz_ros2_control'))
 
     xacro_file = os.path.join(gazebo_ros2_control_demos_path,
                               'urdf',
@@ -51,13 +33,11 @@ def generate_launch_description():
     )
 
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'cartpole'],
+                        arguments=['-topic', 'robot_description','-entity', 'cartpole'],
                         output='screen')
 
     load_joint_state_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
-             'joint_state_broadcaster'],
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start','joint_state_broadcaster'],
         output='screen'
     )
 
--- launch/gripper_mimic_joint.launch.py
+++ launch/gripper_mimic_joint.launch.py
@@ -1,18 +1,3 @@
-# Copyright 2022 Stogl Robotics Consulting UG (haftungsbeschränkt)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Author: Denis Stogl
 
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
@@ -31,7 +16,7 @@ def generate_launch_description():
             " ",
             PathJoinSubstitution(
                 [FindPackageShare(
-                    'gazebo_ros2_control_demos'),
+                    'gz_ros2_control'),
                     'urdf',
                     'gripper_mimic_joint.urdf']
             ),
@@ -53,19 +38,16 @@ def generate_launch_description():
     )
 
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'gripper'],
+                        arguments=['-topic', 'robot_description','-entity', 'gripper'],
                         output='screen')
 
     load_joint_state_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
-             'joint_state_broadcaster'],
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start','joint_state_broadcaster'],
         output='screen'
     )
 
     load_gripper_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
-             'gripper_controller'],
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start','gripper_controller'],
         output='screen'
     )
 
