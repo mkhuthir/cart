52f2be82dd3b33c6887d8d7b70fc1e4bfa01d495
mkhuthir <mkhuthir@gmail.com>
2022-04-19 21:45:38 +0400

    u

--- scripts/py_node.py
+++ scripts/py_node.py
@@ -3,24 +3,25 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from std_msgs.msg import Float64MultiArray    
+
 
 
 class cartPublisher(Node):
 
     def __init__(self):
         super().__init__('my_node')
-        self.publisher_ = self.create_publisher(String, 'my_topic', 10)
-        timer_period = 0.5  # seconds
+        self.publisher_ = self.create_publisher(Float64MultiArray, '/effort_controllers/commands', 10)
+        timer_period = 1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg = Float64MultiArray
+        msg.data[0] = 100
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        self.get_logger().info('Publishing')
+        self.i += 10
 
 
 def main(args=None):
