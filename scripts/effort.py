05c5c0c7eb6412d3b935311c0f2209935e03fb04
mkhuthir <mkhuthir@gmail.com>
2022-04-23 11:22:04 +0400

    u

--- scripts/effort.py
+++ scripts/effort.py
@@ -10,7 +10,7 @@ class cartPublisher(Node):
 
     def __init__(self):
         super().__init__('my_node')
-        self.publisher_ = self.create_publisher(Float64MultiArray, '/effort_controllers/commands', 10)
+        self.pub = self.create_publisher(Float64MultiArray, '/effort_controllers/commands', 10)
         timer_period = 3  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 30.0
@@ -18,7 +18,7 @@ class cartPublisher(Node):
     def timer_callback(self):
         msg = Float64MultiArray()
         msg.data=[self.i]
-        self.publisher_.publish(msg)
+        self.pub.publish(msg)
         self.get_logger().info('Published: "%s"' % msg.data)
         self.i *= -1
 
