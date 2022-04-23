e6022da86f3c2713d8a8d307d1f731519e7d7bdd
mkhuthir <mkhuthir@gmail.com>
2022-04-23 11:13:20 +0400

    u

--- scripts/velocity.py
+++ scripts/velocity.py
@@ -8,16 +8,20 @@ from std_msgs.msg import Float64MultiArray
 class cartPub(Node):
     def __init__(self):
         super().__init__('cartVelocityNode')
+        self.pub = self.create_publisher(Float64MultiArray, '/velocity_controller/commands', 10)
 
 def main(args=None):
     rclpy.init(args=args)
 
-    node = cartPub()
-    
     msg = Float64MultiArray()
     msg.data = [1.0]
-    
+
+    node = cartPub()
+        
     node.get_logger().info("Hello from node")
+    
+    node.pub.publish(msg)
+    node.get_logger().info('Publishing: "%s"' % msg.data)
 
     rclpy.spin(node)
     
